0b03f93f19225ced1408e01481d52a68d1a0cc4f
shimingzhao <34359110+shimingzhao@users.noreply.github.com>
2018-08-23 11:39:20 -0400

    flask-sqlalchemy, flask-migrate, python-dotenv

--- microblog.py
+++ microblog.py
@@ -1,9 +1,20 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
 
 # @app.route('/')
 # def hello_world():
 #     return 'Hello World!'
 #
 #
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
 if __name__ == '__main__':
     app.run()
+
+
